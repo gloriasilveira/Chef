17603f74e9c1087b4684446243c671f0afd87212
Gloria Silveira <gloriasilveira@users.noreply.github.com>
2015-04-02 12:29:56 -0700

    Added email info.

--- cookbooks/Install_SimianArmy/metadata.rb
+++ cookbooks/Install_SimianArmy/metadata.rb
@@ -1,6 +1,6 @@
 name             'Install_SimianArmy'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'SELF'
+maintainer_email 'gloriasilveira@users.noreply.github.com'
 license          'All rights reserved'
 description      'Installs/Configures Install_SimianArmy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
